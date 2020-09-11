03270f36520069780f264075d7ddae527d5aec7d
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-09-10 21:24:05 -0700

    add client-side react

---
+++ build/precache-manifest.c6726c8d9b63fdb3c976a97f969a9fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d08f82f6cd27145f53d8f605b17272c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cf51b17aa305c655177",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "27182d97a6f090ce277c",
+    "url": "/static/css/main.554c1daa.chunk.css"
+  },
+  {
+    "revision": "8cf51b17aa305c655177",
+    "url": "/static/js/2.713809a8.chunk.js"
+  },
+  {
+    "revision": "be3ae91867bd4c806afeb90c76d5e54f",
+    "url": "/static/js/2.713809a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27182d97a6f090ce277c",
+    "url": "/static/js/main.009a9466.chunk.js"
+  },
+  {
+    "revision": "08206a6e16e00551aa99",
+    "url": "/static/js/runtime-main.c9edb931.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const http = require('http')
+const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
@@ -18,10 +19,14 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'build')))
 
 require('./routes/auth-route')(app)
 require('./routes/meal-routes')(app)
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 
 http.createServer(app).listen(PORT, "0.0.0.0", () => { console.log(`Listening on ${PORT}`)})
