c6a150ad218130269516c88cc28a11f183b1c42a
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-11-05 22:49:30 -0800

    tests for POST, PUT and DELETE

--- test/api.js
+++ test/api.js
@@ -317,11 +317,298 @@ describe('meal scheduler server', () => {
           done()
         })
     })
+    it('should fail to schedule a meal without a date', (done) => {
+      let token2 = jwt.sign({
+        username: process.env.LOCAL_USERNAME
+      }, auth.secret, {
+        expiresIn: 43200
+      })
+      let newMeal2 = {
+        meal_desc: 'meal description'
+      }
+      switch(Math.floor(Math.random() * 4 )) {
+        case 0:
+          newMeal2['meal_type'] = 'breakfast'
+          break
+        case 1:
+          newMeal2['meal_type'] = 'lunch'
+          break
+        case 2:
+          newMeal2['meal_type'] = 'dinner'
+          break
+        case 3:
+          newMeal2['meal_type'] = 'snack'
+          break
+      }
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', token2)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(newMeal2)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('No meal date provided.')
+          done()
+        })
+    })
+    it('should fail to schedule a meal without a type', (done) => {
+      let token3 = jwt.sign({
+        username: process.env.LOCAL_USERNAME
+      }, auth.secret, {
+        expiresIn: 43200
+      })
+      let newMeal3 = {
+        meal_date: moment().add(1, 'd').format('MM/DD/YYYY'),
+        meal_desc: 'meal description'
+      }
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', token3)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(newMeal3)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('No meal type provided (breakfast, lunch, dinner, or snack).')
+          done()
+        })
+    })
+    it('should fail to schedule a meal without a description', (done) => {
+      let token4 = jwt.sign({
+        username: process.env.LOCAL_USERNAME
+      }, auth.secret, {
+        expiresIn: 43200
+      })
+      let newMeal4 = {
+        meal_date: moment().add(1, 'd').format('MM/DD/YYYY'),
+      }
+      switch(Math.floor(Math.random() * 4 )) {
+        case 0:
+          newMeal4['meal_type'] = 'breakfast'
+          break
+        case 1:
+          newMeal4['meal_type'] = 'lunch'
+          break
+        case 2:
+          newMeal4['meal_type'] = 'dinner'
+          break
+        case 3:
+          newMeal4['meal_type'] = 'snack'
+          break
+      }
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', token4)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(newMeal4)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('No meal description provided.')
+          done()
+        })
+    })
+    it('should fail to schedule a meal with an invalid date', (done) => {
+      let token5 = jwt.sign({
+        username: process.env.LOCAL_USERNAME
+      }, auth.secret, {
+        expiresIn: 43200
+      })
+      let newMeal5 = {
+        meal_date: moment().add(1, 'd').format('MMMM Do, YYYY'),
+        meal_desc: 'meal description'
+      }
+      switch(Math.floor(Math.random() * 4 )) {
+        case 0:
+          newMeal5['meal_type'] = 'breakfast'
+          break
+        case 1:
+          newMeal5['meal_type'] = 'lunch'
+          break
+        case 2:
+          newMeal5['meal_type'] = 'dinner'
+          break
+        case 3:
+          newMeal5['meal_type'] = 'snack'
+          break
+      }
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', token5)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(newMeal5)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('Invalid date format provided (use MM/DD/YYYY).')
+          done()
+        })
+    })
+    it('should fail to schedule a meal with a blank meal description', (done) => {
+      let token6 = jwt.sign({
+        username: process.env.LOCAL_USERNAME
+      }, auth.secret, {
+        expiresIn: 43200
+      })
+      let newMeal6 = {
+        meal_date: moment().add(2, 'd').format('MM/DD/YYYY'),
+        meal_desc: ''
+      }
+      switch(Math.floor(Math.random() * 4 )) {
+        case 0:
+          newMeal6['meal_type'] = 'breakfast'
+          break
+        case 1:
+          newMeal6['meal_type'] = 'lunch'
+          break
+        case 2:
+          newMeal6['meal_type'] = 'dinner'
+          break
+        case 3:
+          newMeal6['meal_type'] = 'snack'
+          break
+      }
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', token6)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(newMeal6)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('No meal description provided.')
+          done()
+        })
+    })
   })
   describe('PUT /api/meals', () => {
-
+    var putToken = jwt.sign({
+      username: process.env.LOCAL_USERNAME
+    }, auth.secret, {
+      expiresIn: 43200
+    })
+    var putType = Math.floor(Math.random() * 4 )
+    var putMeal = {
+      meal_date: moment().add(1, 'd').format('MM/DD/YYYY'),
+      meal_desc: 'test description',
+      meal_type: putType === 0 ? putType === 1 ? putType === 2 ? 'breakfast' : 'lunch' : 'dinner' : 'snack'
+    }
+    beforeEach((done) => {
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', putToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(putMeal)
+        .end((err, res) => {
+          done()
+        })
+    })
+    afterEach((done) => {
+      chai.request(server)
+        .delete('/api/meals')
+        .set('x-access-token', putToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(putMeal)
+        .end((err, res) => {
+          done()
+        })
+    })
+    it('should successfully update an existing meal when all fields are provided', (done) => {
+      let put1 = {
+        meal_date: putMeal.meal_date,
+        meal_type: putMeal.meal_type,
+        meal_desc: 'modified'
+      }
+      chai.request(server)
+        .put('/api/meals')
+        .set('x-access-token', putToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(put1)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('Meal details updated successfully!')
+          done()
+        })
+    })
+    it('should fail to update if the meal does not already exist', (done) => {
+      let newDate = moment().add(1, 'w').format('MM/DD/YYYY')
+      let put2 = {
+        meal_date: newDate,
+        meal_type: putMeal.meal_type,
+        meal_desc: 'modified'
+      }
+      chai.request(server)
+        .put('/api/meals')
+        .set('x-access-token', putToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(put2)
+        .end((err, res) => {
+          res.should.have.status(404)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal(`Could not update as no ${putMeal.meal_type} currently exists for ${newDate}.`)
+          done()
+        })
+    })
   })
   describe('DELETE /api/meals', () => {
+    var delToken = jwt.sign({
+      username: process.env.LOCAL_USERNAME
+    }, auth.secret, {
+      expiresIn: 43200
+    })
+    var delType = Math.floor(Math.random() * 4 )
+    var delMeal = {
+      meal_date: moment().add(1, 'd').format('MM/DD/YYYY'),
+      meal_desc: 'test description',
+      meal_type: delType === 0 ? delType === 1 ? delType === 2 ? 'breakfast' : 'lunch' : 'dinner' : 'snack'
+    }
+    before((done) => {
+      chai.request(server)
+        .post('/api/meals')
+        .set('x-access-token', delToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(delMeal)
+        .end((err, res) => {
+          done()
+        })
+    })
+    it('should successfully delete an existing meal when all fields are provided', (done) => {
+      chai.request(server)
+        .delete('/api/meals')
+        .set('x-access-token', delToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(delMeal)
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal('Meal successfully deleted.')
+          done()
+        })
+    })
+    it('should fail to delete the meal if it does not already exist', (done) => {
+      chai.request(server)
+        .delete('/api/meals')
+        .set('x-access-token', delToken)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(delMeal)
+        .end((err, res) => {
+          res.should.have.status(404)
+          res.should.be.json
+          res.body.should.be.a('object')
+          res.body.message.should.equal(`This meal does not exist so was not deleted.`)
+          done()
+        })
+    })
 
   })
   describe('GET /api/users', () => {
